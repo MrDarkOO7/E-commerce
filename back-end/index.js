b3a44485e581f4536d5d389ca21bde64a50de8ad
Muhammad Waleed <m.waleedapsacian@gmail.com>
2024-07-04 18:16:27 +0500

    minor changes

--- back-end/index.js
+++ back-end/index.js
@@ -7,8 +7,9 @@ const connectDB = async () => {
   mongoose.connect("mongodb://localhost:27017");
   const productSchema = new mongoose.Schema({});
   const product = mongoose.model("Product", productSchema);
-  const data = await productSchema.find();
+  const data = await product.find();
   console.warn(data);
 };
+connectDB();
 
 app.listen(5000);
