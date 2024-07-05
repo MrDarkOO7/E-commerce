8f7c062e5bf5627717ea71dcee9042ede2e36ccf
Muhammad Waleed <m.waleedapsacian@gmail.com>
2024-07-05 16:59:00 +0500

    cors error removal

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("./db/config");
 const User = require("./db/users");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
