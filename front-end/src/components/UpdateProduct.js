6462eeb27f0bdd264ba3dc6da612ec493a5d9faf
Muhammad Waleed <m.waleedapsacian@gmail.com>
2024-07-23 03:07:37 +0500

    update api integrated

--- front-end/src/components/UpdateProduct.js
+++ front-end/src/components/UpdateProduct.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const params = useParams();
+  const navigate = useNavigate();
 
-  const updateProduct = (e) => {
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    let result = await fetch(`http://localhost:5000/product/${params.id}`);
+    result = await result.json();
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
+
+  const updateProduct = async (e) => {
     e.preventDefault();
-    console.log(name, price, category, company);
+    //console.log(name, price, category, company);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`, {
+      method: "PUT",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: { "Content-Type": "application/json" },
+    });
+    result = await result.json();
+    navigate("/");
   };
 
   return (
@@ -20,7 +43,7 @@ const UpdateProduct = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Enter Product Name"
+              value={name}
               required
               onChange={(e) => setName(e.target.value)}
             />
@@ -29,7 +52,7 @@ const UpdateProduct = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Enter Product Price"
+              value={price}
               required
               onChange={(e) => setPrice(e.target.value)}
             />
@@ -38,7 +61,7 @@ const UpdateProduct = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Enter Product Category"
+              value={category}
               required
               onChange={(e) => setCategory(e.target.value)}
             />
@@ -47,7 +70,7 @@ const UpdateProduct = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Enter Product Company"
+              value={company}
               required
               onChange={(e) => setCompany(e.target.value)}
             />
