a8a0ba498eede5c34092f48f48129a3b1a03349c
Muhammad Waleed <m.waleedapsacian@gmail.com>
2024-07-23 21:37:08 +0500

    middleware for jwt authorization

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,8 @@ require("./db/config");
 const User = require("./db/users");
 const Product = require("./db/product");
 const cors = require("cors");
+const Jwt = require("jsonwebtoken");
+const jwtkey = "e-commerce";
 
 const app = express();
 
@@ -15,7 +17,12 @@ app.post("/register", async (req, res) => {
   result = result.toObject();
 
   delete result.password;
-  res.send(result);
+  Jwt.sign({ result }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      res.send("somethign went wrong");
+    }
+    res.send({ result, auth: token });
+  });
 });
 
 app.post("/login", async (req, res) => {
@@ -23,7 +30,12 @@ app.post("/login", async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      res.send(user);
+      Jwt.sign({ user }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          res.send("somethign went wrong");
+        }
+        res.send({ user, auth: token });
+      });
     } else {
       res.send({ result: "User not found" });
     }
@@ -32,19 +44,19 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post("/add-product", async (req, res) => {
+app.post("/add-product", verifyToken, async (req, res) => {
   let product = new Product(req.body);
   let result = await product.save();
   res.send(result);
 });
 
-app.get("/products", async (req, res) => {
+app.get("/products", verifyToken, async (req, res) => {
   let products = await Product.find();
   if (products.length > 0) res.send(products);
   else res.send({ result: "No products found" });
 });
 
-app.get("/product/:id", async (req, res) => {
+app.get("/product/:id", verifyToken, async (req, res) => {
   let result = await Product.findOne({ _id: req.params.id });
   if (result) {
     res.send(result);
@@ -53,13 +65,13 @@ app.get("/product/:id", async (req, res) => {
   }
 });
 
-app.delete("/product/:id", async (req, res) => {
+app.delete("/product/:id", verifyToken, async (req, res) => {
   //res.send(req.params.id);
   const result = await Product.deleteOne({ _id: req.params.id });
   res.send(result);
 });
 
-app.put("/product/:id", async (req, res) => {
+app.put("/product/:id", verifyToken, async (req, res) => {
   const result = await Product.updateOne(
     { _id: req.params.id },
     { $set: req.body }
@@ -67,7 +79,7 @@ app.put("/product/:id", async (req, res) => {
   res.send(result);
 });
 
-app.get("/search/:key", async (req, res) => {
+app.get("/search/:key", verifyToken, async (req, res) => {
   const result = await Product.find({
     $or: [
       { name: { $regex: req.params.key, $options: "i" } },
@@ -77,5 +89,20 @@ app.get("/search/:key", async (req, res) => {
   });
   res.send(result);
 });
+
+function verifyToken(req, res, next) {
+  const token = req.headers["authorization"];
+  if (token) {
+    Jwt.verify(token, jwtkey, (err, valid) => {
+      if (err) {
+        res.send({ result: "Provide a valid token" });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.send({ result: "Provide a token with the header" });
+  }
+}
 
 app.listen(5000);
